d78510dba81717b7d6a8761b29122addc3281f5a
mrenee73 <michelecountryman1@gmail.com>
2021-05-13 09:01:51 -0400

    final push for assignment

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 
 
 
-
 const api_url = 'https://api.covid19api.com/summary';
-const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
+submitBtn.addEventListener('submit', fetchResults);
+let countryArray;
 
-// async function getInfo() {
-//     const response = await fetch(api_url);
-//     const data = await response.json();    
-//     console.log(data);
-// }
+const fetchResults = () => {
 
-// getInfo();
+ fetch(api_url)
+        .then ((res) => res.json())
+        .then (data => {
+            countryArray = data.Countries[181]
+           console.log(data.Countries[181]);
+           displayData(data.Countries[181])
+            
+        })              
+ };
+const factsDisplay = document.querySelector(".myData");
+ function displayData(countryArray){
+     
+
+         let div = document.createElement('div');
+         let ctry = document.createElement("h3");
+         let newC = document.createElement("p");
+         let newD = document.createElement("p");
+         let totalCC = document.createElement("p");
+         let totalD = document.createElement("p");
+
+        ctry.text = countryArray;
+
+        console.log(function);
+        factsDisplay.appendChild(div);
+        div.appendChild(ctry);        
+
+        // ctry.innerHTML = 'Country:' + thing[i].Country;
+        // newC.innerHTML = 'New Cases:' + thing[i].NewConfirmed;
+        // newD.innerHTML = 'New Death:' + thing[i].NewDeaths;
+        // totalCC.innerHTML = 'Total Confirmed Cases:' + thing[i].TotalConfirmed;
+        // totalD.innerHTML = 'Total Deaths:' + thing[i].TotalDeaths;
 
 
 
-searchForm.addEventListener('submit', fetchResults);
-
-function fetchResults(e) {
-    // console.log(e);
-    e.preventDefault();
-    url = `${api_url}&countries${countries}`;
-    
-   
-    fetch(url)
-        .then(function (result) {
-            console.log(result)
-            return result.json();
-        })
-        .then(function (json) {
-            console.log(json);
-            displayResults(json);
-        })
-}
-
-function displayResults(json) {
-    console.log('Display Results', json);
-    
-}
+           
+ };
 
 
-searchForm.addEventListener('submit', fetchResults);
-fetchResults();
 
